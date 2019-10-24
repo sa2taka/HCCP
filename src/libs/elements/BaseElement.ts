e83411162c9a70f8fab785c638246db1bba31522
sa2taka <sa2taka.com>
2019-10-24 01:29:37 +0000

    :sparkles: Change something of base element

--- src/libs/elements/BaseElement.ts
+++ src/libs/elements/BaseElement.ts
@@ -1,40 +1,51 @@
 import ContentCategoryType from '@/libs/elements/ContentCategoryType';
 
-export default abstract class BaseElement {
+export default class BaseElement {
   public name: string = '';
-  public _childrenModel:
+  public _childrenModels:
     | [ContentCategoryType | string]
     | ContentCategoryType.Empty = ContentCategoryType.Empty;
-  public _parentsModel:
+  public _parentsModels:
     | [ContentCategoryType | string]
     | ContentCategoryType.Empty = ContentCategoryType.Empty;
   public parent: BaseElement | undefined;
   public children: [BaseElement] | undefined;
-  public attribute: Record<string, string> = {};
+  public attributes: Record<string, string>;
 
-  constructor(parent?: BaseElement, children?: [BaseElement]) {
+  constructor(
+    name: string,
+    childrenModels: [ContentCategoryType | string] | ContentCategoryType.Empty,
+    parentsModels: [ContentCategoryType | string] | ContentCategoryType.Empty,
+    parent?: BaseElement,
+    children?: [BaseElement],
+    attribute: Record<string, string> = {}
+  ) {
+    this.name = name;
+    this._childrenModels = childrenModels;
+    this._parentsModels = parentsModels;
     this.parent = parent;
     this.children = children;
+    this.attributes = attribute;
   }
 
-  public get parentsModel():
+  public get parentsModels():
     | [ContentCategoryType | string]
     | ContentCategoryType.Empty {
-    if (this._parentsModel.includes(ContentCategoryType.Transparent)) {
+    if (this._parentsModels.includes(ContentCategoryType.Transparent)) {
       if (typeof this.parent !== 'undefined') {
-        return this.parent.parentsModel;
+        return this.parent.parentsModels;
       } else {
         return ContentCategoryType.Empty;
       }
     } else {
-      return this._parentsModel;
+      return this._parentsModels;
     }
   }
 
-  public get childrenModel():
+  public get childrenModels():
     | [ContentCategoryType | string]
     | ContentCategoryType.Empty {
-    return this._childrenModel;
+    return this._childrenModels;
   }
 
   public appendChild(child: BaseElement) {
