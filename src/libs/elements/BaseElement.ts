53fd691fa903a1945c02cb430dc536a1f4b92aa3
sa2taka <sa2taka.com>
2019-10-24 02:22:08 +0000

    :sparkles: Add to judge valid

--- src/libs/elements/BaseElement.ts
+++ src/libs/elements/BaseElement.ts
@@ -2,52 +2,55 @@ import ContentCategoryType from '@/libs/elements/ContentCategoryType';
 
 export default class BaseElement {
   public name: string = '';
-  public _childrenModels:
+  public _contentModel:
     | [ContentCategoryType | string]
     | ContentCategoryType.Empty = ContentCategoryType.Empty;
-  public _parentsModels:
-    | [ContentCategoryType | string]
-    | ContentCategoryType.Empty = ContentCategoryType.Empty;
+  public _context: [ContentCategoryType | string] | ContentCategoryType.Empty =
+    ContentCategoryType.Empty;
   public parent: BaseElement | undefined;
   public children: [BaseElement] | undefined;
   public attributes: Record<string, string>;
 
   constructor(
     name: string,
-    childrenModels: [ContentCategoryType | string] | ContentCategoryType.Empty,
-    parentsModels: [ContentCategoryType | string] | ContentCategoryType.Empty,
+    contentModel: [ContentCategoryType | string] | ContentCategoryType.Empty,
+    context: [ContentCategoryType | string] | ContentCategoryType.Empty,
     parent?: BaseElement,
     children?: [BaseElement],
     attribute: Record<string, string> = {}
   ) {
     this.name = name;
-    this._childrenModels = childrenModels;
-    this._parentsModels = parentsModels;
+    this._contentModel = contentModel;
+    this._context = context;
     this.parent = parent;
     this.children = children;
     this.attributes = attribute;
   }
 
-  public get parentsModels():
+  public get contentModel():
     | [ContentCategoryType | string]
     | ContentCategoryType.Empty {
-    if (this._parentsModels.includes(ContentCategoryType.Transparent)) {
+    if (this._contentModel.includes(ContentCategoryType.Transparent)) {
       if (typeof this.parent !== 'undefined') {
-        return this.parent.parentsModels;
+        return this.parent.context;
       } else {
         return ContentCategoryType.Empty;
       }
     } else {
-      return this._parentsModels;
+      return this._contentModel;
     }
   }
 
-  public get childrenModels():
+  // The following code is general.
+  // Special scenes are implemented in inheritance.
+  public get context():
     | [ContentCategoryType | string]
     | ContentCategoryType.Empty {
-    return this._childrenModels;
+    return this._context;
   }
 
+  // The following code is general.
+  // Special scenes are implemented in inheritance.
   public appendChild(child: BaseElement) {
     if (typeof this.children !== 'undefined') {
       this.children.push(child);
@@ -56,5 +59,40 @@ export default class BaseElement {
     }
   }
 
-  abstract isValid(): boolean;
+  //
+  public isValid(): boolean {
+    if (typeof this.parent === 'undefined') {
+      return this.isValidAsParentIsEmpty();
+    }
+
+    // The following code is general.
+    // Special scenes are implemented in inheritance.
+    const parentAllowModels = this.parent.contentModel;
+
+    // If parent's children model(category model) is empty(nothing),
+    // element is basically invalid.
+    // An exception is the 'template' element.
+    if (parentAllowModels === ContentCategoryType.Empty) {
+      return false;
+    }
+
+    return parentAllowModels.some((model) => {
+      if (typeof model === 'string') {
+        return this.name === model;
+      } else {
+        return this.context.includes(model);
+      }
+    });
+  }
+
+  private isValidAsParentIsEmpty(): boolean {
+    if (
+      this.contentModel !== ContentCategoryType.Empty &&
+      this.contentModel.every((elm) => typeof elm === 'string')
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
---
+++ src/libs/elements/ElementBuilder.ts
@@ -0,0 +1,1 @@
+import BaseElements from '@/libs/elements/BaseElement';
