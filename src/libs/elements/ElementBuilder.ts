85109631258c7c9d6c36fbfc30cf464fceb7470b
sa2taka <sa2taka.com>
2019-10-24 03:09:30 +0000

    :sparkles: Add builder and add p tag builder for example

--- src/libs/elements/ElementBuilder.ts
+++ src/libs/elements/ElementBuilder.ts
@@ -1,1 +1,56 @@
-import BaseElements from '@/libs/elements/BaseElement';
+import BaseElement from '@/libs/elements/BaseElement';
+import ContentCategoryType from '@/libs/elements/ContentCategoryType';
+
+namespace ElementBuilder {
+  const build = (
+    name: string,
+    parent?: BaseElement,
+    children?: [BaseElement],
+    attribute: Record<string, string> = {}
+  ): BaseElement => {
+    const specialElement = buildSpecialElement(
+      name,
+      parent,
+      children,
+      attribute
+    );
+
+    if (specialElement) {
+      return specialElement;
+    }
+
+    const { context, contentModel } = Relationship[name];
+
+    return new BaseElement(
+      name,
+      contentModel,
+      context,
+      parent,
+      children,
+      attribute
+    );
+  };
+
+  const buildSpecialElement = (
+    name: string,
+    parent?: BaseElement,
+    children?: [BaseElement],
+    attribute: Record<string, string> = {}
+  ): BaseElement | null => {
+    // TODO write code to build special element
+    return null;
+  };
+
+  interface Category {
+    context: [ContentCategoryType | string] | ContentCategoryType.Empty;
+    contentModel: [ContentCategoryType | string] | ContentCategoryType.Empty;
+  }
+
+  // Relationship Between Element And Category
+  const Relationship: Record<string, Category> = {
+    a: {
+      context: [ContentCategoryType.Phrasing],
+      contentModel: [ContentCategoryType.Transparent],
+    },
+  };
+}
